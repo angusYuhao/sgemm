656dd13b6d849dd398d42079a340df58c0ceb959
Angus Wang <wangjadehao@gmail.com>
2025-12-23 16:25:37 -0800

    add boundary checks for shared mem blocking kernel

--- sgemm/csrc/kernels/shared_memory_blocking.cuh
+++ sgemm/csrc/kernels/shared_memory_blocking.cuh
@@ -19,24 +19,36 @@ sgemm_shared_memory_blocking_kernel(uint32_t M, uint32_t N, uint32_t K,
   const uint32_t tx = threadIdx.x, ty = threadIdx.y;
 
   // get position of C matrix that this thread is computing for
-  const uint32_t row = bx * BLOCK_SIZE + tx;
-  const uint32_t col = by * BLOCK_SIZE + ty;
+  const uint32_t row = by * BLOCK_SIZE + ty;
+  const uint32_t col = bx * BLOCK_SIZE + tx;
 
   // outer loop: loop through the blocks in the K direction
   float tmp = 0.0;
   for (int blk_id = 0; blk_id < CEIL_DIV(K, BLOCK_SIZE); blk_id++) {
     // cooperatively load A and B elements from global memory
-    A_blk[tx][ty] = A[row * K + blk_id * BLOCK_SIZE + ty];
-    B_blk[tx][ty] = B[(blk_id * BLOCK_SIZE + tx) * N + col];
+    if (row < M && (blk_id * BLOCK_SIZE + tx) < K) {
+      A_blk[ty][tx] = A[row * K + blk_id * BLOCK_SIZE + tx];
+    }
+    else {
+      A_blk[ty][tx] = 0.0f;
+    }
+    if ((blk_id * BLOCK_SIZE + ty) < K && col < N) {
+      B_blk[ty][tx] = B[(blk_id * BLOCK_SIZE + ty) * N + col];
+    }
+    else {
+      B_blk[ty][tx] = 0.0f;
+    }
     __syncthreads();
 
     // inner loop
     for (int k = 0; k < BLOCK_SIZE; k++) {
-      tmp += A_blk[tx][k] * B_blk[k][ty];
+      tmp += A_blk[ty][k] * B_blk[k][tx];
     }
     __syncthreads();
   }
 
   // populate the output tensor
-  C[row * N + col] = alpha * tmp + beta * C[row * N + col];
+  if (row < M && col < N){
+    C[row * N + col] = alpha * tmp + beta * C[row * N + col];
+  }
 }
--- sgemm/csrc/sgemm.cu
+++ sgemm/csrc/sgemm.cu
@@ -24,14 +24,14 @@ void sgemm_global_memory_coalesce_v1_cuda(uint32_t M, uint32_t N, uint32_t K, co
 
 void sgemm_global_memory_coalesce_v2_cuda(uint32_t M, uint32_t N, uint32_t K, const float *A,
                                           const float *B, float *C, float alpha, float beta) {
-  dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32));
+  dim3 gridDim(CEIL_DIV(N, 32), CEIL_DIV(M, 32));
   dim3 blockDim(32, 32);
   sgemm_global_memory_coalesce_v2_kernel<<<gridDim, blockDim>>>(M, N, K, alpha, A, B, beta, C);
 }
 
 void sgemm_shared_memory_blocking_cuda(uint32_t M, uint32_t N, uint32_t K, const float *A,
                                        const float *B, float *C, float alpha, float beta) {
-  dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32));
+  dim3 gridDim(CEIL_DIV(N, 32), CEIL_DIV(M, 32));
   dim3 blockDim(32, 32);
   sgemm_shared_memory_blocking_kernel<32><<<gridDim, blockDim>>>(M, N, K, alpha, A, B, beta, C);
 }
